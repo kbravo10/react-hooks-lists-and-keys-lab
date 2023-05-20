0c6d9b6fded631d50bc0408d14d1a5ecaf525762
Kevin Bravo <kbravo1024@gmail.com>
2023-05-19 17:15:17 -0700

    complete lost and keys lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const span = technologies.map((techs,index) =>{
+    return <span key={index}>{techs }</span>
+  })
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {span}
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,16 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  const project = projects.map((projectObj) =>{
+    return <ProjectItem key={projectObj.id} name={projectObj.name} about={projectObj.anout} technologies={projectObj.technologies} />
+  })
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {project}
+      </div>
     </div>
   );
 }
